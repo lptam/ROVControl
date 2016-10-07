64ad780955f1afc9ada0bc2011721b61da3e11e1
Luiz Paulo <eng.lpss@gmail.com>
2016-10-07 11:27:03 -0300

    modificação do código de detecção de objeto

--- objectdetect.cpp
+++ objectdetect.cpp
@@ -1,42 +1,19 @@
 #if defined(__linux__) || defined(LINUX) || defined(__APPLE__) || defined(ANDROID)
 
-#include <opencv2/imgproc.hpp>  // Gaussian Blur
-#include <opencv2/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
+#include <opencv2/imgproc.hpp> 
+#include <opencv2/core.hpp>    
 #include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>  // OpenCV window I/O
+#include <opencv2/highgui.hpp> 
 #include <opencv2/features2d.hpp>
 #include <opencv2/objdetect.hpp>
-
 #include <stdio.h>
 #include <string>
 #include <vector>
-
-
-
-
-
-
-
 #include<stdlib.h>
 #include<iostream>
 #include<ctime>
 #include<unistd.h>
 #include<sys/types.h>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 using namespace std;
 using namespace cv;
